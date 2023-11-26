8a932d829c4b91fe665c21ee5facc85da01cea7a
Michal Jan Warecki <mwarecki1@gmail.com>
2023-11-26 12:31:50 +0100

    Pull from Git if a GIT_PATH is given.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+if [ $GIT_PATH ]; do
+	echo "Syncing from Git"
+	if [ -d /srv/.git ]; then
+		cd /srv
+		git pull
+	else
+		git clone $GIT_PATH /srv
+	fi
+else
+	echo "Environment variable GIT_PATH is not defined, not syncing from Git."
+fi
+
+echo "Starting dnsmasq"
 dnsmasq -k
